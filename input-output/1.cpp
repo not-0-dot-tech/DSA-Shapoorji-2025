2d9f4a8549c47a5f976e5a150ad1f77291eb2aad
Alik Agarwala <alikagarwala@outlook.com>
2025-09-06 22:19:01 +0530

    Add C programs for basic input and output handling

---
+++ input-output/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    // cin >> a;
+    // cin >> b;
+
+    cin >> a >> b; // This is equivalent to the above two lines
+
+    // cout << "Sum is: " << a + b;
+    // cout << endl; // endl is used to insert a new line
+
+    cout << "Sum is: " << a + b << endl; // This is equivalent to the above two lines
+
+    return 0;
+}
---
+++ input-output/2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char ch;
+    float f;
+    string str;
+
+    cin >> a >> ch >> f >> str; // Input: 10 A 20.5 Hello
+
+    cout << "Integer: " << a << endl;
+    cout << "Character: " << ch << endl;
+    cout << "Float: " << f << endl;
+    cout << "String: " << str << endl;
+
+    return 0;
+}
