7f57c2077add579f8f429957f44912762dfb9a85
Alik Agarwala <alikagarwala@outlook.com>
2025-09-09 21:38:41 +0530

    Add C++ program to demonstrate basic if-else conditional statements

---
+++ if-else/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool flag = true; // You can change this to false to see the other output
+    // true and false can also be written as 1 and 0 respectively
+
+    if (flag)
+    {
+        cout << "Hello World" << endl;
+    }
+    else
+    {
+        cout << "Hello Universe" << endl;
+    }
+    return 0;
+}
