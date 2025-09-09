ebb0cb8a09007107ce405b7a35042a8406f46568
Alik Agarwala <alikagarwala@outlook.com>
2025-09-09 21:46:00 +0530

    Add C program to demonstrate the use of boolean data type

---
+++ booleans/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    // int flag = 1;
+    //_Bool flag = 1;
+    bool flag = true;
+    // true and false can also be written as 1 and 0 respectively
+
+    return 0;
+}
