f5a971e7d1d6725f81af99e0d991270d7715a7cf
Alik Agarwala <alikagarwala@outlook.com>
2025-09-09 22:23:54 +0530

    Add C++ program demonstrating an infinite loop with a semicolon

---
+++ loop/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int i = 5;
+
+    while (i <= 5)
+        ; // Infinite loop due to semicolon
+
+    cout << i << " ";
+    return 0;
+}
+
+
