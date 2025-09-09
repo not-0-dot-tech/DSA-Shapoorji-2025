5f41239c34a0983f90188e45720a4d35ea76752f
Alik Agarwala <alikagarwala@outlook.com>
2025-09-09 22:03:30 +0530

    Add C++ program to check for vowels using switch-case

---
+++ switch-case/4.cpp
@@ -0,0 +1,30 @@
+// check for vowel (Simulation of Logical OR using switch-case)
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter an alphabet: ";
+    cin >> ch;
+
+    switch (ch)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        cout << ch << " is a vowel." << endl;
+        break;
+    default:
+        cout << ch << " is a consonant." << endl;
+    }
+
+    return 0;
+}
